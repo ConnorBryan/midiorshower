27d42f1dd75a5a6c8686931652e955b6802d43d1
Connor Bryan <cchromium@gmail.com>
2020-10-27 17:52:53 -0500

    Initial setup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 import { loadGame } from "./game";
 
+// Store
+const slice = createSlice({
+  name: "slice",
+  initialState: {},
+  reducers: {},
+});
+
+// App
 export default React.memo(
   function App() {
-    const gameContainer = React.useRef<null | HTMLElement>(null);
-    const game = React.useRef<null | Phaser.Game>(null);
+    const gameContainer = useRef<null | HTMLElement>(null);
+    const game = useRef<null | Phaser.Game>(null);
+    const store = useRef(
+      configureStore({
+        reducer: slice.reducer,
+      })
+    );
 
     useEffect(() => {
       if (gameContainer.current && !game.current) {
         game.current = loadGame(gameContainer.current);
+
+        setImmediate(() => {
+          game
+            .current!.scene.getScenes()
+            .forEach((scene) => scene.data.set("store", store));
+        });
       }
     });
 
